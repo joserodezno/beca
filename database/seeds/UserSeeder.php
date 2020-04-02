33198f48809205da75371b4989592bfc59b3f3e2
Jose Rodezno <joserodezno@joses-macbook-pro.home>
2020-04-02 15:09:32 +0200

    enlazar usuario con id de profesion

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,15 +12,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $professions = DB::select('SELECT id FROM professions WHERE title = ?', ['Desarrollador back-end']);
+        //$professions = DB::select('SELECT id FROM professions WHERE title = ? LIMIT 0,1', ['Desarrollador back-end']);
 
-        dd($professions[0]->id);
+        $professionId = DB::table('professions')
+            ->whereTitle('Desarrollador back-end')
+            ->value('id');
 
         DB::table('users')->insert([
             'name' => 'Jose Rodezno',
             'email' => 'joserodezno99@gmail.com',
             'password' => bcrypt('prueba'),
-            'profession_id' => $professions[0]->id,
+            'profession_id' => $professionId
         ]);
     }
 }
