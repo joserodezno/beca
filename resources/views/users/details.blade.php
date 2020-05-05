f5350be5be43fe03a1046495717d7e7cb468edd0
Jose Rodezno <joserodezno@joses-macbook-pro.home>
2020-05-05 13:48:46 +0200

    generar url dinamicas

--- resources/views/users/details.blade.php
+++ resources/views/users/details.blade.php
@@ -5,10 +5,12 @@
 
 @section('content')
 
-<h1>Usuario #{{$user->id}}</h1>
+    <h1>Usuario #{{$user->id}}</h1>
 
-
- 
     <p>Nombre del usuario: {{ $user->name }}</p>
     <p>Correo electronico: {{ $user->email }}</p>
+
+    <p>
+    <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
+    </p>
 @endsection
