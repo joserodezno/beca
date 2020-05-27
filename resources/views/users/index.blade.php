d856bc0703bd899d77f708dc7840fa6b90eafbf5
Jose Rodezno <joserodezno@joses-macbook-pro.home>
2020-05-27 13:45:52 +0200

    mejora visual html

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,11 +7,25 @@
     <p>
         <a href="{{ route('users.create') }}">Nuevo usuario</a>
     </p>
-     
-    <ul>
-        @forelse ($users as $user) 
-            <li>
-                {{ $user->name }}, ({{ $user->email}})
+
+    @if ($users->isNotEmpty())
+
+    <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+          <tr>
+            <th scope="row">{{ $user->id }}</th>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>
                 <a href="{{ route('users.details', $user) }}">Ver detalles </a> | 
                 <a href="{{ route('users.edit', $user) }}">Editar</a> |
                 <form action="{{ route('users.destroy', $user) }}" method="POST">
@@ -19,12 +33,14 @@
                     {{ method_field('DELETE') }}
                     <button type="submit">Eliminar</button>
                 </form>
-            </li>
-        @empty       
-            <li>No hay usuarios registrados.</li>
-        @endforelse
-    </ul>
-
+            </td>
+          </tr>
+        @endforeach
+        </tbody>
+      </table>
+      @else
+        <p>No hay usuarios registrados.</p>
+      @endif
 @endsection
 
 @section('sidebar')
