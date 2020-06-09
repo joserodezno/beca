761690307d3dfeb50cd7072b5955b60428f5f01b
Jose Rodezno <joserodezno@joses-macbook-pro.home>
2020-06-09 13:28:02 +0200

    mejora pagina de detalles

--- resources/views/users/details.blade.php
+++ resources/views/users/details.blade.php
@@ -5,12 +5,17 @@
 
 @section('content')
 
-    <h1>Usuario #{{$user->id}}</h1>
-
-    <p>Nombre del usuario: {{ $user->name }}</p>
-    <p>Correo electronico: {{ $user->email }}</p>
-
-    <p>
-    <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
-    </p>
+    <div class="card">
+       <h4 class="card-header">Usuario #{{$user->id}}</h4>
+        <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Nombre del usuario: {{ $user->name }}</li>
+                <li class="list-group-item">Correo electronico: {{ $user->email }}</li>
+                <li class="list-group-item"><a href="{{ route('users.index') }}">Regresar al listado de usuarios</a></li>
+              </ul>
+              
+        </div>
+    
+    </div>
+    
 @endsection
